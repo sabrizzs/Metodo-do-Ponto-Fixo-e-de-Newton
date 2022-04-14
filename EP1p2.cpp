1f023fa5393ed6dca64d3dc3a60a8543808ee478
sabrizzs <93349105+sabrizzs@users.noreply.github.com>
2022-04-14 16:06:43 -0300

    Update EP1p2.cpp

--- EP1p2.cpp
+++ EP1p2.cpp
@@ -2,9 +2,30 @@
 #include <cmath> //biblioteca math.h
 using namespace std;
 
-/* parte 2
+/*  parte 2
     Método de Newton
+    Algoritmo que gera uma imagem que ilustra as bacias de convergência de uma função.
 */
+
+//calcula o valor da função f aplicada em x
+void evalf(double x){
+    return;
+}
+
+//calcula o valor da derivada da função f aplicada em x
+void evalDf(double x){
+    return;
+}
+
+//acha as bacias de convergência da função f no domínio [l1, u1]x[l2, u2] e gera um arquivo output.txt que contém os dados para a geração da imagem das bacias
+void newton_basis(double l, double u, double p){
+    return;
+}
+
+//aplica o método de Newton para achar uma raiz da função f (com primeira derivada f0), partindo do ponto x0.
+void newton(double xo){
+    return;
+}
 
 void parteii(){
     return;
